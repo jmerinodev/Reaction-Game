90dfb1ceac95dee1932fa4739d4b201579ede887
Web Dev Journey <jmerino.dev@gmail.com>
2022-08-17 21:33:43 +0200

    upload project

---
+++ game.js
@@ -0,0 +1,49 @@
+var btn = document.getElementById('btn');
+
+var timer = 0;
+var timeout;
+var interval;
+
+btn.addEventListener('click', startGame, false);
+
+function startGame() {
+    btn.removeEventListener('click', startGame);
+    inProgress();  
+}
+
+function inProgress() {
+    btn.addEventListener('click', pressed, false);
+    btn.classList.add("in-progress");
+    btn.innerHTML = "<h1>... <br/> Wait for Green</h1>";
+    var miliseconds = Math.floor(Math.random() * (15 - 10 + 10) + 10) * 100;
+    timeout = setTimeout(function() {
+        btn.classList.add("press-now");
+        btn.innerHTML = "<h1>... <br/> Click!</h1>";
+        interval = setInterval(function() {
+            timer++;
+        }, 1);
+    }, miliseconds);
+}
+
+function pressed() {
+    clearInterval(interval);
+    clearTimeout(timeout);
+    btn.removeAttribute('class');
+    if (timer == 0) {
+        btn.classList.add('primary');
+        btn.innerHTML = "<h1>Too soon!</h1><p>Click to try again.</p>"
+    }
+    else {
+        btn.classList.add('primary');
+        btn.innerHTML = "<h1>" + (timer * 10) + " ms</h1>" + "<p>Click to keep going.</p>";
+    }
+    btn.removeEventListener('click', pressed, false);
+    btn.addEventListener('click', restartGame, false);
+}
+
+function restartGame() {
+    timer = 0;
+    btn.removeAttribute('class');
+    btn.removeEventListener('click', restartGame, false);
+    inProgress();
+}
